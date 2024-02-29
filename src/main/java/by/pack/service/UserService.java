a99274f29d8be8a711d8164e288adf398fe169f5
Skrit-ex <skritex0@gmail.com>
2024-02-29 21:11:29 +0300

    added

--- src/main/java/by/pack/service/UserService.java
+++ src/main/java/by/pack/service/UserService.java
@@ -1,10 +1,15 @@
 package by.pack.service;
 
 import by.pack.dao.HibernateUserDao;
+import by.pack.dto.LoginUserDto;
 import by.pack.dto.RegUserDto;
+import by.pack.entity.SessionUser;
 import by.pack.entity.User;
 import by.pack.mapper.RegUserDtoMapper;
+import by.pack.mapper.UserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Optional;
 
 public class UserService {
 
@@ -14,5 +19,18 @@ public class UserService {
     public void save(RegUserDto regUserDto){
         User user = RegUserDtoMapper.RegUserToUser(regUserDto);
         hibernateUserDao.save(user);
+    }
+
+    public Optional<SessionUser>login(LoginUserDto loginUserDto){
+        Optional<User> user =hibernateUserDao.findByEmail(loginUserDto.getEmail());
+        if(user.isPresent()){
+
+            User currentUser =user.get();
+
+            if(currentUser.getPassword().equals(loginUserDto.getPassword())){
+                return Optional.of(UserMapper.userToSessionUser(currentUser));
+            }
+        }
+        return Optional.empty();
     }
 }
